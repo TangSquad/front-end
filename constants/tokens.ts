e22ffdddd25908f9ea9f6bdebfd63be6eed5c828
Jeonghyeon Lee <dlqjdgus99@naver.com>
2024-07-18 23:32:32 +0900

    design: create tokens.ts

---
+++ constants/tokens.ts
@@ -0,0 +1,12 @@
+const rg_12 = 'font-rg tracking-rg text-xxsm leading-xxsm';
+const md_10 = 'font-md tracking-md text-xxxsm leading-xxxsm';
+const md_12 = 'font-md tracking-md text-xxsm leading-xxsm';
+const bd_16 ='font-bd tracking-bd text-sm leading-sm';
+
+
+export default {
+  rg_12,
+  md_10,
+  md_12,
+  bd_16,
+};
