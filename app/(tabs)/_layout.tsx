ff0064fb4f073481e1a91fc4f4d8f749e28643f5
Jeonghyeon Lee <dlqjdgus99@naver.com>
2024-07-30 23:24:12 +0900

    style: get rid of unnecessary React Fragment

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -25,79 +25,78 @@ const TabIcon = ({ icon, color, name }: TabIconProps) => {
   );
 };
 
-function TabLayout() {
+function TabLayout(): JSX.Element {
   return (
-    <>
-      <Tabs
-        screenOptions={{
-          tabBarShowLabel: false,
-          tabBarActiveTintColor: '#0A54FF',
-          tabBarInactiveTintColor: '#575E70',
-          tabBarStyle: {
-            height: 90,
-            borderTopWidth: 1,
-            borderTopColor: '#EEF1F6',
-          },
+    <Tabs
+      screenOptions={{
+        lazy: true,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: '#0A54FF',
+        tabBarInactiveTintColor: '#575E70',
+        tabBarStyle: {
+          height: 90,
+          borderTopWidth: 1,
+          borderTopColor: '#EEF1F6',
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: 'Home',
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <TabIcon 
+              icon={icons.home}
+              color={color}
+              name="홈"
+            />
+          ),
         }}
-      >
-        <Tabs.Screen
-          name="home"
-          options={{
-            title: 'Home',
-            headerShown: false,
-            tabBarIcon: ({ color }) => (
-              <TabIcon 
-                icon={icons.home}
-                color={color}
-                name="홈"
-              />
-            ),
-          }}
-        />
-        <Tabs.Screen 
-          name="search"
-          options={{
-            title: 'Search',
-            headerShown: false,
-            tabBarIcon: ({ color }) => (
-              <TabIcon 
-                icon={icons.search}
-                color={color}
-                name="찾기"
-              />
-            ),
-          }}
-        />
-        <Tabs.Screen 
-          name="logbook"
-          options={{
-            title: 'Logbook',
-            headerShown: false,
-            tabBarIcon: ({ color }) => (
-              <TabIcon 
-                icon={icons.logbook}
-                color={color}
-                name="로그북"
-              />
-            ),
-          }}
-        />
-        <Tabs.Screen 
-          name="profile"
-          options={{
-            title: 'Profile',
-            headerShown: false,
-            tabBarIcon: ({ color }) => (
-              <TabIcon 
-                icon={icons.profile}
-                color={color}
-                name="프로필"
-              />
-            ),
-          }}
-        />
-      </Tabs>
-    </>
+      />
+      <Tabs.Screen 
+        name="search"
+        options={{
+          title: 'Search',
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <TabIcon 
+              icon={icons.search}
+              color={color}
+              name="찾기"
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen 
+        name="logbook"
+        options={{
+          title: 'Logbook',
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <TabIcon 
+              icon={icons.logbook}
+              color={color}
+              name="로그북"
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen 
+        name="profile"
+        options={{
+          title: 'Profile',
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <TabIcon 
+              icon={icons.profile}
+              color={color}
+              name="프로필"
+            />
+          ),
+        }}
+      />
+    </Tabs>
   );
 };
 
