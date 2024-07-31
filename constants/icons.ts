66c6f904d78672aec69bae3bbe5b54862298a387
Jeonghyeon Lee <dlqjdgus99@naver.com>
2024-07-31 22:14:40 +0900

    chore: add plus icon

--- constants/icons.ts
+++ constants/icons.ts
@@ -4,6 +4,7 @@ import logbook from '../assets/icons/logbook.png';
 import profile from '../assets/icons/profile.png';
 import group from '../assets/icons/group.png';
 import diving from '../assets/icons/diving.png';
+import plus from '../assets/icons/plus.png';
 
 export default {
   home,
@@ -12,4 +13,5 @@ export default {
   profile,
   group,
   diving,
+  plus,
 };
