6bca6eb2a48f78f57cddf23a2e69a32bef3be353
Jeonghyeon Lee <dlqjdgus99@naver.com>
2024-08-15 23:45:04 +0900

    chore: add add-photo.png and images in constants directory

---
+++ constants/images.ts
@@ -0,0 +1,7 @@
+import defaultImage from './icons/default.png';
+import addPhoto from './icons/add-photo.png';
+
+export default {
+  defaultImage,
+  addPhoto,
+};
--- constants/index.ts
+++ constants/index.ts
@@ -1,5 +1,7 @@
 import icons from './icons';
+import images from './images';
 import tokens from './tokens';
 
 export { icons };
+export { images };
 export { tokens };
