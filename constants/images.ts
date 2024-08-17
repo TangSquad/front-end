302defac9b42cbf9ed8a1cf5d9bac03d8c0422ca
Jeonghyeon Lee <dlqjdgus99@naver.com>
2024-08-17 23:44:01 +0900

    fix: paths in images.ts

--- constants/images.ts
+++ constants/images.ts
@@ -1,5 +1,5 @@
-import defaultImage from './icons/default.png';
-import addPhoto from './icons/add-photo.png';
+import defaultImage from '../assets/images/default-image.png';
+import addPhoto from '../assets/images/add-photo.png';
 
 export default {
   defaultImage,
