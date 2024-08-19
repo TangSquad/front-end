ad3fbda080262a894886de9a54248b11429f5a71
Jeonghyeon Lee <dlqjdgus99@naver.com>
2024-08-20 01:30:35 +0900

    feat: toggle password shown

--- components/Auth/CustomInput.tsx
+++ components/Auth/CustomInput.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
-import { TextInput, View, Text } from 'react-native';
+import { TextInput, View, Text, TouchableOpacity, Image } from 'react-native';
 import { tokens } from '../../constants';
+import { icons } from '../../constants';
 
 interface CustomInputProps {
   title: string;
@@ -11,19 +12,32 @@ interface CustomInputProps {
 
 function CustomInput({ title, type, placeholder, setInput }: CustomInputProps) {
   const [isFocused, setIsFocused] = useState(false);
+  const [showPswd, setShowPswd] = useState(false);
+
+  const togglePswd = () => {
+    setShowPswd(!showPswd);
+  };
 
   return (
     <View className='flex items-start w-full h-fit mb-26'>
       <Text className={`${tokens.bd_16} color-primary`}>{title}</Text>
-      <TextInput
-        className={`w-full bg-white ${isFocused ? 'border-primary-500' : 'border-gray-300'} border-b-[1px] py-10`}
-        autoCapitalize='none'
-        placeholder={placeholder}
-        placeholderTextColor={tokens.gray_400}
-        onChangeText={(value) => setInput(value.trim())}
-        onFocus={() => setIsFocused(true)}
-        onBlur={() => setIsFocused(false)}
-      />
+      <View className={`flex-row items-center justify-between w-full relative ${isFocused ? 'border-primary-500' : 'border-gray-300'} border-b-[1px]`}>
+        <TextInput
+          className='flex-1 bg-white py-10'
+          autoCapitalize='none'
+          placeholder={placeholder}
+          placeholderTextColor={tokens.gray_400}
+          onChangeText={(value) => setInput(value.trim())}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+          secureTextEntry={type === 'password' && !showPswd}
+        />
+        {type === 'password' && (
+          <TouchableOpacity onPress={togglePswd} className='ml-10'>
+            <Image className='w-24 h-24' resizeMode='contain' source={ showPswd ? icons.openedEyes : icons.closedEyes} />
+          </TouchableOpacity>)
+        }
+      </View>
     </View>
   );
 }
