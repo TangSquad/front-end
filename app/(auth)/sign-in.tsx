1889e1db07d3fc4aad709bb67209ed3aae7aa9fa
Jeonghyeon Lee <dlqjdgus99@naver.com>
2024-08-21 02:58:11 +0900

    fix: handle keypad not disappearing on blur on sign-in page

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView, Text, TouchableOpacity, View, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import { useMutation } from '@tanstack/react-query';
 import { router } from 'expo-router';
 import CustomInput from '../../components/Auth/CustomInput';
@@ -31,30 +31,32 @@ function SignIn() {
   };
 
   return (
-    <SafeAreaView className='bg-white h-full'>
-      <View className='flex items-center w-full px-26'>
-        <Text className={`${tokens.bd_28} color-primary-600 mt-50 mb-60`}>TANG SQUAD</Text>
-        <CustomInput
-          title="이메일"
-          type="email"
-          placeholder='이메일을 입력해주세요.'
-          setInput={setEmail}
-        />
-        <CustomInput
-          title="비밀번호"
-          type="password"
-          placeholder='비밀번호를 입력해주세요.'
-          setInput={setPassword}
-        />
-        <TouchableOpacity
-          className='flex items-center justify-center w-full h-60 bg-primary rounded-10'
-          activeOpacity={0.8}
-          onPress={emailSignin}
-        >
-          <Text className={`${tokens.bd_16} color-white`}>로그인</Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false} touchSoundDisabled={true}>
+      <SafeAreaView className='bg-white h-full'>
+        <View className='flex items-center w-full px-26'>
+          <Text className={`${tokens.bd_28} color-primary-600 mt-50 mb-60`}>TANG SQUAD</Text>
+          <CustomInput
+            title="이메일"
+            type="email"
+            placeholder='이메일을 입력해주세요.'
+            setInput={setEmail}
+          />
+          <CustomInput
+            title="비밀번호"
+            type="password"
+            placeholder='비밀번호를 입력해주세요.'
+            setInput={setPassword}
+          />
+          <TouchableOpacity
+            className='flex items-center justify-center w-full h-60 bg-primary rounded-10'
+            activeOpacity={0.8}
+            onPress={emailSignin}
+          >
+            <Text className={`${tokens.bd_16} color-white`}>로그인</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
   );
 }
 
