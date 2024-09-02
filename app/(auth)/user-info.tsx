1d072efb0b1fa318dd9d78dfe55e4c5d628c7911
Jeonghyeon Lee <dlqjdgus99@naver.com>
2024-09-02 12:34:41 +0900

    feat: nickname input on user-info page

--- app/(auth)/user-info.tsx
+++ app/(auth)/user-info.tsx
@@ -1,13 +1,16 @@
 import { useState } from 'react';
 import { View, Text } from 'react-native';
 import ProfileSelector from '../../components/Auth/ProfileSelector';
+import { TitledInput } from '../../components/Auth/SignupInput';
 
 function UserInfo() {
   const [uri, setUri] = useState<string | null>(null);
+  const [nickname, setNickname] = useState<string>('');
 
   return (
     <View className='flex-1 items-center bg-white p-24'>
       <ProfileSelector uri={uri} setUri={setUri} />
+      <TitledInput type='nickname' setInput={setNickname} />
     </View>
   );
 }
