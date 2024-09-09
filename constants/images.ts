9283d518c5732b20c392a1244a6713eabb1b26ab
Jeonghyeon Lee <dlqjdgus99@naver.com>
2024-09-09 16:58:08 +0900

    chore: remove organization images

--- constants/images.ts
+++ constants/images.ts
@@ -2,24 +2,10 @@ import defaultImage from '../assets/images/default-image.png';
 import addPhoto from '../assets/images/add-photo.png';
 import logo from '../assets/images/logo.png';
 import profileDefault from '../assets/images/profile-default.png';
-import PADI from '../assets/images/organizations/padi.png';
-import SDI from '../assets/images/organizations/sdi.png';
-import CMAS from '../assets/images/organizations/cmas.png';
-import SSI from '../assets/images/organizations/ssi.png';
-import NAUI from '../assets/images/organizations/naui.png';
-import KUDA from '../assets/images/organizations/kuda.png';
-import BSAC from '../assets/images/organizations/bsac.png';
 
 export default {
   defaultImage,
   addPhoto,
   logo,
   profileDefault,
-  PADI,
-  SDI,
-  CMAS,
-  SSI,
-  NAUI,
-  KUDA,
-  BSAC,
 };
