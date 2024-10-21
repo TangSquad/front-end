cc9ff45441eca8a19160663f5ed80eb2fb78c77c
Jeonghyeon Lee <dlqjdgus99@naver.com>
2024-10-21 23:09:59 +0900

    feat: diving-details page

--- app/(screens)/diving/[id].tsx
+++ app/(screens)/diving/[id].tsx
@@ -1,7 +1,8 @@
-import { Text, View, SafeAreaView } from 'react-native';
+import { Text, View, SafeAreaView, Image } from 'react-native';
 import { useQuery } from '@tanstack/react-query';
 import { useLocalSearchParams } from 'expo-router';
 import { getDivingById } from 'api/diving/diving';
+import { tokens, icons } from 'constants/';
 
 export default function DivingDetails() {
   const { id } = useLocalSearchParams() as { id: string };
@@ -12,8 +13,13 @@ export default function DivingDetails() {
   });
 
   return (
-    <SafeAreaView>
-      <Text>Diving Details</Text>
+    // ScrollView to be added
+    <SafeAreaView className='items-center bg-gray-50'>
+      <Image source={icons.defaultPhoto} className='mt-[40] mb-100' />
+      <View className='h-full w-full bg-white p-24 rounded-20'>  
+        <Text className={`${tokens.bd_24} color-gray-800`}>{data?.divingName}Test</Text>
+        <Text className={`${tokens.md_14} color-gray-600`}>{data?.divingIntro}test</Text>
+      </View>
     </SafeAreaView>
   );
 }
