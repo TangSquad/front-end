004a5abcf6d98c58c154b664d10bfcb28ed8f27f
Jeonghyeon Lee <dlqjdgus99@naver.com>
2024-11-05 21:41:03 +0900

    feat: add more on gathering-details page

--- app/(screens)/diving/[id].tsx
+++ app/(screens)/diving/[id].tsx
@@ -17,6 +17,25 @@ export default function DivingDetails() {
     <SafeAreaView className='items-center bg-gray-50'>
       <Image source={icons.defaultPhoto} className='mt-[40] mb-100' />
       <View className='h-full w-full bg-white p-24 rounded-20'>  
+        <View className='flex-row justify-between mb-26'>
+          <View className='flex-row'>
+            {/* location, license */}
+            <Text className={`${tokens.md_12} color-gray-500`}>{data?.location}</Text>
+            <Text className={`${tokens.rg_12} color-gray-500`}>자격조건</Text>
+            <Text className={`${tokens.md_12} color-gray-500`}>{data?.limitLicense}</Text>
+          </View>
+          <View className='flex-row'>
+            {/* member, like-btn */}
+            <View className={`flex-row items-center ${tokens.md_12} color-gray-500`}>
+              <Image source={icons.member} className='mr-4' />
+              <Text className={`${tokens.rg_14} color-gray-500`}>2/{data?.limitPeople}</Text>
+            </View>
+            {/* like-btn to be added */}
+          </View>
+        </View>
+        <Text className={`${tokens.md_16} color-primary`}>
+          # {data?.age} # {data?.moodOne} # {data?.moodTwo}
+        </Text>
         <Text className={`${tokens.bd_24} color-gray-800`}>{data?.divingName}Diving Test</Text>
         <Text className={`${tokens.md_14} color-gray-600`}>{data?.divingIntro}diving test</Text>
       </View>
--- app/(screens)/moim/[id].tsx
+++ app/(screens)/moim/[id].tsx
@@ -1,15 +1,16 @@
 import { Text, View, SafeAreaView, Image } from 'react-native';
 import { useQuery } from '@tanstack/react-query';
 import { useLocalSearchParams } from 'expo-router';
-import { getDivingById } from 'api/diving/diving';
+import { getMoimById } from 'api/moim/moim';
+import MainButton from 'components/common/MainButton';
 import { tokens, icons } from 'constants/';
 
 export default function MoimDetails() {
   const { id } = useLocalSearchParams() as { id: string };
 
   const { data, error } = useQuery({
-    queryKey: ['moimById'],
-    queryFn: () => getDivingById(Number(id)),
+    queryKey: ['moimById', id],
+    queryFn: () => getMoimById(Number(id)),
   });
 
   return (
@@ -17,8 +18,34 @@ export default function MoimDetails() {
     <SafeAreaView className='items-center bg-gray-50'>
       <Image source={icons.defaultPhoto} className='mt-[40] mb-100' />
       <View className='h-full w-full bg-white p-24 rounded-20'>  
-        <Text className={`${tokens.bd_24} color-gray-800`}>{data?.divingName}Moim Test</Text>
-        <Text className={`${tokens.md_14} color-gray-600`}>{data?.divingIntro}moim test</Text>
+        <View className='flex-row justify-between mb-26'>
+          <View className='flex-row'>
+            {/* location, license */}
+            <Text className={`${tokens.md_12} color-gray-500`}>{data?.locationOne}</Text>
+            <Text className={`${tokens.rg_12} color-gray-500`}>자격조건</Text>
+            <Text className={`${tokens.md_12} color-gray-500`}>{data?.licenseLimit}</Text>
+          </View>
+          <View className='flex-row'>
+            {/* member, like-btn */}
+            <View className={`flex-row items-center ${tokens.md_12} color-gray-500`}>
+              <Image source={icons.member} className='mr-4' />
+              <Text className={`${tokens.rg_14} color-gray-500`}>2/{data?.limitPeople}</Text>
+            </View>
+            {/* like-btn to be added */}
+          </View>
+        </View>
+        <Text className={`${tokens.md_16} color-primary`}>
+          # {data?.age} # {data?.moodOne} # {data?.moodTwo}
+        </Text>
+        <Text className={`${tokens.bd_24} color-gray-800`}>{data?.moimName}Moim Test title</Text>
+        <Text className={`${tokens.md_14} color-gray-600`}>{data?.moimIntro}moim test intro</Text>
+        <Text className={`${tokens.md_14} color-gray-600`}>{data?.moimDetails}moim test details</Text>
+        <Text className={`${tokens.bd_20} color-gray-700 mt-50`}>1인 예상 활동 비용</Text>
+        <View className='h-200'></View>
+        <MainButton
+          title='참여하기'
+          handlePress={() => {}}
+        />
       </View>
     </SafeAreaView>
   );
