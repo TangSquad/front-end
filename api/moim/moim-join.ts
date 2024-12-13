b0406c5b4ee2b2490bb7c1411e86e1e9b17991af
Jeonghyeon Lee <dlqjdgus99@naver.com>
2024-12-14 02:36:58 +0900

    fix: request method and response type of joinMoim

--- api/moim/moim-join.ts
+++ api/moim/moim-join.ts
@@ -3,20 +3,18 @@ import apiClient from '../apiClient';
 import { api } from 'constants/';
 
 interface Response {
-  success: boolean;
-  message: string;
-  data: null;
+  registeredUserIds: string[];
 }
 
 const joinMoim = async (moimId: number) => {
   try {
-    const response = await apiClient.put<Response>(api.ENDPOINTS.MOIM.MOIM_JOIN.replace('{moimId}', `${moimId}`));
+    const response = await apiClient.post<Response>(api.ENDPOINTS.MOIM.MOIM_JOIN.replace('{moimId}', `${moimId}`));
     return response.data;
   } catch (error) {
     if (axios.isAxiosError(error))
-      throw new Error(error.response?.data.message || 'Failed to get joined moim');
+      throw new Error(error.response?.data.message || 'Axios failed to join moim');
     else
-      throw new Error('Failed to get joined moim');
+      throw new Error('Failed to join moim');
   }
 };
 
