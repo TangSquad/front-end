3f53d2ac6a1885802358af264905b882632abbde
Jeonghyeon Lee <dlqjdgus99@naver.com>
2025-04-13 01:39:08 -0300

    feat: greeting page

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,20 @@
 import { Text, View } from 'react-native';
 import { useEffect } from 'react';
 import { router } from 'expo-router';
+import { tokens } from 'constants/';
 
 export default function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
-      router.push('/(auth)/sign-in');
+      router.push('/sign-in');
     }, 3000);
     
     return () => clearTimeout(timer);
   }, []);
   return (
-    <View className="w-screen h-screen items-center justify-center bg-white font-sans-md">
-      <Text>GREET!</Text>
+    <View className="w-screen h-screen items-center justify-center bg-primary">
+      <Text className={`${tokens.bd_28} color-white`}>TANG SQUAD</Text>
+      <Text className={`${tokens.bd_16} color-primary-100`}>다이버와 세상의 모든 연결의 시작</Text>
     </View>
   );
 }
